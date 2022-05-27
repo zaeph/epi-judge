9296e64d07ab53a19f91dc2cb6a2f62e93ee5cbc
Leo Vivier <zaeph@zaeph.net>
2022-05-27 09:39:45 +0200

    Early solution for 4.1

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,11 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    p = 0
+    while x:
+        p ^= x & 1
+        x >>= 1
+    return p
 
 
 if __name__ == '__main__':
