4a40874a537710dcf44a1694df15b192c3dd5c90
Leo Vivier <zaeph@zaeph.net>
2022-05-28 10:11:22 +0200

    4.1: Add other solutions and implement helper for testing

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,7 +1,7 @@
 from test_framework import generic_test
 
 
-def parity(x: int) -> int:
+def parity_bruteforce(x: int) -> int:
     p = 0
     while x:
         p ^= x & 1
@@ -9,5 +9,31 @@ def parity(x: int) -> int:
     return p
 
 
+def parity_bruteforce_refined(x: int) -> int:
+    p = 0
+    while x:
+        p ^= 1
+        x = x & x - 1
+    return p
+
+
+def parity(x: int) -> int:
+    x ^= x >> 32
+    x ^= x >> 16
+    x ^= x >> 8
+    x ^= x >> 4
+    x ^= x >> 2
+    x ^= x >> 1
+    return x & 0x1
+
+
+def test(s: str) -> None:
+    print(f'{s}')
+    generic_test.generic_test_main('parity.py', 'parity.tsv', eval(s))
+    print('----------------------------------------')
+
+
 if __name__ == '__main__':
-    exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
+    test('parity_bruteforce')
+    test('parity_bruteforce_refined')
+    test('parity')
